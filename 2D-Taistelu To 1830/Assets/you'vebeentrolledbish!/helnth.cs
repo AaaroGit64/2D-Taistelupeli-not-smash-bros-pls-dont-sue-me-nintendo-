7bd14078ba7aedf529f57f1651dbd2062517bc56
SnaksiXD <ataunimaa@gmail.com>
2022-02-17 19:31:40 +0200

    jump(notthesong)

---
+++ 2D-Taistelu To 1830/Assets/you'vebeentrolledbish!/helnth.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helnth : MonoBehaviour
+{
+    public float maxhealth = 100;
+
+    public float health;
+
+    public float HitTimer = 0.15f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- 2D-Taistelu To 1830/Assets/you'vebeentrolledbish!/scripts4scrubslikeme.cs
+++ 2D-Taistelu To 1830/Assets/you'vebeentrolledbish!/scripts4scrubslikeme.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class scripts4scrubslikeme : MonoBehaviour
 {
     public float speed = 10f;
-    public float jumpForce = 15f;
+    public float jumpForce = 5f;
     public Rigidbody2D myRigidbody2D;
 
     public CircleCollider2D feet;
     private float horizontalMovement = 0f;
     public int facing = 1;
+    
+    public LayerMask PoopLMAO;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +24,11 @@ public class scripts4scrubslikeme : MonoBehaviour
     void Update()
     {
         horizontalMovement = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump") && feet.IsTouchingLayers(PoopLMAO))
+        {
+
+            myRigidbody2D.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
     }
 
     void FixedUpdate()
