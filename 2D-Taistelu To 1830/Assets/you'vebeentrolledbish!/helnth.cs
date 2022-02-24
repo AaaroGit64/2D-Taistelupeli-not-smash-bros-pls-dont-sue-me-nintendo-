544b84eaa1c3ddff5f03e89cedc8ef6d7e790733
SnaksiXD <ataunimaa@gmail.com>
2022-02-24 19:33:34 +0200

    helthandmentalbreakdowns

--- 2D-Taistelu To 1830/Assets/you'vebeentrolledbish!/helnth.cs
+++ 2D-Taistelu To 1830/Assets/you'vebeentrolledbish!/helnth.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,15 +10,61 @@ public class helnth : MonoBehaviour
     public float health;
 
     public float HitTimer = 0.15f;
+
+    public bool isHit = false;
+
+    public Rigidbody2D myRigidbody;
+    scripts4scrubslikeme moveScript;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        moveScript = GetComponent<scripts4scrubslikeme>();
+        health = maxhealth;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       if (health <= 0);
+       {
+            Die();
+
+       }
+       
     }
+
+    private void Die()
+    {
+        // make hurt
+    }
+
+    public void GetSmashedIntoOblivion(float philSwift)
+    {
+        if(!isHit)
+        { 
+            // now that's a lotta damage
+            health  -= philSwift;
+            StartCoroutine(Knockback());
+
+        }
+       
+        
+
+    }
+
+    IEnumerator Knockback()
+    {
+        //Käskyt välittömästi
+        isHit = true;
+        myRigidbody.velocity = new Vector2(moveScript.facing * -2.5f, 2.5f);
+        //kalkulator
+        yield return new WaitForSeconds(HitTimer);
+        //Ajastimen jälkeen tapahtuvat käskyt
+        isHit = false;
+
+    }
+
 }
